be654f3bb10b35c07142c097e89dcd244457e784
joao garcia <jiboiadamor@gmail.com>
2022-10-10 10:54:26 +0100

    created a plus and minus setCount

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import Counter from './components/Counter';
+
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <Counter />
+    </div>
   );
 }
 
---
+++ src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleMinus = () => {
+    setCount(count - 1)
+  };
+
+  const handlePlus = () => {
+    setCount(count + 1)
+  };
+
+  return <div className="m-3 d-flex align-items-center justify-content-center">
+    <button className="btn btn-danger me-2" onClick={handleMinus}>-</button>
+    <span className="fs-5">{count}</span>
+    <button className="btn btn-success ms-2" onClick={handlePlus}>+</button>
+  </div>
+};
+
+export default Counter;
