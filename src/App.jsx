19b5febedc30e8e1be50a530f282b53a7bb8d3e0
joao garcia <jiboiadamor@gmail.com>
2022-10-10 12:38:57 +0100

    added bootstrap icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import Counter from './components/Counter';
 
 const App = () => {
+  const [counters, setCounters] = useState([
+    {id: 1, start: 10, step: 6 },
+    {id: 2, start: 5 },
+    {id: 3, start: 0, step: 13 }
+  ])
+
   return (
     <div>
-      <Counter start={10} step={6} />
-      <Counter start={10} />
+      {counters.map((counter) => {
+        return <Counter start={counter.start} step={counter.step} />
+      })}
     </div>
   );
 }
